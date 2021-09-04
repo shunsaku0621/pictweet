b696c1eadd370b98545519349812600a4be3217f
shunsaku0621 <hamagi.0621@icloud.com>
2021-09-04 21:48:20 +0900

    first-commit

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # root 'tweets#index'
+  # resources :tweets, only: [:index,:new,:create,:destroy,:edit,:update,:show]
+
+  
+  root to:'tweets#index'
+  resources :tweets do 
+    resources :comments, only: :create
+    collection do
+      get 'search'
+    end
+  end
+  
+  resources :users, only: [:show]
+
+
+  
+  
+
+end
+
+
+
+
---
+++ spec/requests/tweets_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe "Tweets", type: :request do
+
+  before do
+    @tweet = FactoryBot.create(:tweet)
+  end
+
+  # describe "GET /tweets" do
+  #   it "works! (now write some real specs)" do
+  #     get tweets_index_path
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
+
+
+
+  describe 'GET #index' do
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get root_path
+      # binding.pry
+      expect(response.status).to eq 200
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+      get root_path
+      # binding.pry
+      expect(response.body).to include(@tweet.text)
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
+      get root_path
+      expect(response.body).to include(@tweet.image)
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
+      get root_path
+      expect(response.body).to include('投稿を検索する')
+    end
+  end
+
+
+
+
+
+  describe 'GET #show' do
+
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get tweet_path(@tweet)
+      expect(response.status).to eq 200
+    end
+
+    it 'showアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+      get tweet_path(@tweet)
+      expect(response.body).to include(@tweet.text)
+    end
+
+    it 'showアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
+      get tweet_path(@tweet)
+      expect(response.body).to include(@tweet.image)
+    end
+
+    it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
+      get tweet_path(@tweet)
+      expect(response.body).to include('＜コメント一覧＞')
+    end
+
+  end
+
+
+
+
+end
